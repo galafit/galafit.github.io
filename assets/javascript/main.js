842c0165c0a7efc205b096cecdb518a44fd27f7b
Gala <galafit@gmail.com>
2014-05-04 09:02:16 +0200

    first finished (complete and working) version

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,136 +1,144 @@
 
-// Set equal heights of .thumbnails inside of the .thumbnail-container
-// (Originally  had set equal heights of columns (divs) inside of the .container)
-function setEqualHeight(columns) {
-    var tallestcolumn = 0;
-    columns.each(
-        function () {
-            currentHeight = $(this).height();
-            if (currentHeight > tallestcolumn) {
-                tallestcolumn = currentHeight;
-            }
-        }
-    );
-    columns.height(tallestcolumn);
+//----------------------------------------------------
+
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
 }
-$(document).ready(function () {
-    // setEqualHeight($(".container > div"));
-    setEqualHeight($(".thumbnail-container .thumbnail"));
-});
 
 
 //  Show #toTop button only after scrolling page dawn a little ( 150 px )
-$(function () {
+function showToTopButton() {
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 150) {
+        if ( ($(this).scrollTop() > 150) ) {
             $('#toTop').fadeIn();
         } else {
             $('#toTop').fadeOut();
         }
     });
-});
+}
+
+/************************************************************************************
+ Set equal height of some elements inside the container
+ *************************************************************************************/
 
 
-//  Activate fancybox to show images and galleries
-$(document).ready(function () {
-    $(".fancybox").fancybox();
-});
+
+function equalHeight(container, element){
+		var heightArray = container.find(element).map( function(){
+				 return  $(this).height();
+				 }).get();
+		var maxHeight = Math.max.apply( Math, heightArray);
+    container.find(element).height(maxHeight);
+			}
+
+function setEqualHeight(container, element) {
+    $(container).add(element).height("auto");
+
+    var bigScreen = 1000;
+    if($(document).width() >= bigScreen) {
+        $(container).each( function(){
+            equalHeight( $(this), element);
+        });
+    }
+}
+
 
 
 /************************************************************************************
  Color Theme Change
  *************************************************************************************/
 
-function setCookie (name, value, expires, path, domain, secure) {
-      document.cookie = name + "=" + value +
-        ((expires) ? "; expires=" + expires : "") +
-        ((path) ? "; path=" + path : "") +
-        ((domain) ? "; domain=" + domain : "") +
-        ((secure) ? "; secure" : "");
-}
+function setCookie(name, value) {
+    $.cookie(name, value);
 
+    $.cookie(name, null);
+    $.cookie(name, value, {
+        expires: 360,
+        path: '/'
+    });
+
+}
 
 
 function getCookie(name) {
-    var cookie = " " + document.cookie;
-    var search = " " + name + "=";
-    var setStr = null;
-    var offset = 0;
-    var end = 0;
-    if (cookie.length > 0) {
-        offset = cookie.indexOf(search);
-        if (offset != -1) {
-            offset += search.length;
-            end = cookie.indexOf(";", offset)
-            if (end == -1) {
-                end = cookie.length;
-            }
-            setStr = cookie.substring(offset, end);
-        }
-    }
-    return(setStr);
+    return  $.cookie(name);
 }
 
-function setCookie_(name, value){
-    $.cookie(name, value);
-}
-
-function getCookie_(name){
-    $.cookie(name);
-}
-
-
-
-
-
-function setTheme(theme) {
-    //el = $(document.body);
-    el = $(document.documentElement);
-    el.removeClass('theme-base-intense');
-    el.removeClass('theme-inverse');
-    el.removeClass('theme-inverse-intense');
-    el.addClass(theme);
-    setCookie("theme", theme);
-}
 
 function changeTheme() {
-    el = $(document.documentElement);
+    //el = $(document.body);
+    // el = $(document.documentElement);
+    el = $("html");
 
-    if (el.hasClass('theme-inverse-intense')) {
-        el.removeClass('theme-inverse-intense');
-        el.addClass("theme-base-intense");
-        setCookie("theme", "base", "", '/', 'mixdreams.com','');
-    }
-    else if (el.hasClass('theme-inverse')) {
+    if (el.hasClass('theme-inverse')) {
         el.removeClass('theme-inverse');
-        ssetCookie("theme", "base", "", '/', 'mixdreams.com','');
-    }
-    else if (el.hasClass('theme-base-intense')) {
-        el.removeClass('theme-base-intense');
-        el.addClass("theme-inverse-intense");
-        setCookie("theme", "inverse", "", '/', 'mixdreams.com','');
+        setCookie("mixdreams-theme", "base");
     }
     else {
         el.addClass("theme-inverse");
-        setCookie("theme", "inverse", "", '/', 'mixdreams.com','');
+        setCookie("mixdreams-theme", "inverse");
     }
+
+    setEqualHeight($(".thumbnail-container"), $(".thumbnail"));
 }
 
-function init() {
-    cookie = getCookie("theme");
-    el = $(document.documentElement);
-    if (cookie == "inverse") {
-        if (el.hasClass('theme-base-intense')) {
-            el.removeClass('theme-base-intense');
-            el.addClass("theme-inverse-intense");
-        }
-        else if (!(el.hasClass('theme-inverse')) && !(el.hasClass('theme-inverse-intense'))) {
+
+function setTheme() {
+    cookie = getCookie("mixdreams-theme")
+    el = $("html");
+    if (!el.hasClass('theme-base') && !el.hasClass('theme-inverse')) {
+        if (cookie == "inverse") {
             el.addClass("theme-inverse");
         }
     }
 }
 
-init();
+$(window).resize(function() {
+    setEqualHeight($(".thumbnail-container"), $(".thumbnail"));
+ });
+
+$('.change-theme').on('click', function() {
+     changeTheme();
+ });
+
+ $('#menu').on('click', function(e) {
+         e.preventDefault();
+         $('.nav--adaptive').toggleClass('open');
+ });
+
+
+
+
+$(document).ready(function () {
+   setEqualHeight($(".thumbnail-container"), $(".thumbnail"));
+    setTheme();
+    showToTopButton();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
